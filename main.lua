0da51e74faa3e34c77c7a6c72cd8620608f119c3
Meowybot <jorgedidier149@gmail.com>
2025-10-09 16:06:31 -0600

    add variables

--- main.lua
+++ main.lua
@@ -1,4 +1,11 @@
 function love.load()
+    --set up initial variables
+    catgame = {}
+    catgame.game = {}
+    catgame.game.hp = 50
+    catgame.game.misses = 0
+    catgame.game.note1 = {}
+    catgame.game.note1.color = "white"
 end
 
 function love.update(dt)
