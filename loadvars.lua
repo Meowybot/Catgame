0fb192c7be2510ce23745446e7b46e7f2449797e
Meowybot <jorgedidier149@gmail.com>
2025-10-11 10:48:27 -0600

    make loadvars.lua

--- loadvars.lua
+++ loadvars.lua
@@ -1,4 +1,11 @@
-function love.load()
-    --variables
-    --switch to menu
+catsing = {}
+catsing.level = {}
+catsing.level.hp = 50
+catsing.level.miss = 0
+catsing.level.loaded = "level01"
+
+function catsing.level:load(name)
+    catsing.level.loaded = name
+    state.switch("assets/levels/" .. name)
+    --todo add other stuff
 end
