4727088345c3590c7fc9a3e70c64896a3f20f33b
Meowybot <jorgedidier149@gmail.com>
2025-10-11 09:32:46 -0600

    deleted main.lua vars to load in loadvars

--- main.lua
+++ main.lua
@@ -1,27 +1,10 @@
 function love.load()
-    --set up initial variables
-    catgame = {}
-    catgame.game = {}
-    catgame.game.hp = 50
-    catgame.game.misses = 0
-    catgame.game.note1 = {}
-    catgame.game.note1.color = "white"
+    state = require("lib/stateswitcher")
+    state.switch("states/loadvars")
 end
 
 function love.update(dt)
 end
 
 function love.draw()
-end
-
---custom functions:
-
-function addhp(hp, maxhp)
-    catgame.game.hp = catgame.game.hp + hp
-    if catgame.game.hp > maxhp then
-        catgame.game.hp = maxhp
-    end
-end
-
-function ongameclick(x, y)
 end
