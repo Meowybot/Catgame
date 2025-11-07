b8b35cb3c07ad9eab9c978ad0f81c90c0172f829
Meowybot <jorgedidier149@gmail.com>
2025-11-07 15:01:40 -0600

    Actualizar level.lua

--- catsing/level.lua
+++ catsing/level.lua
@@ -8,6 +8,10 @@ level.mdata.secps = level.mdata.secpb / level.mdata.spb
 level.mdata.name = "Tutorial"
 level.mdata.id = "00"
 level.mdata.curstep = 0
+level.stats = {}
+level.stats.miss = 0
+level.stats.hp = 100
+level.stats.score = 0
 
 function level.mdata.setup(name, id, bpm, spb)
     level.mdata.bpm = bpm or 120
@@ -36,12 +40,18 @@ function level.notes.add(number, step)
     local time_to_hit = step * level.mdata.secps
     newnote.x = initial_spawn_offset_x + (time_to_hit * speed_per_second)
     newnote.r = 25
+    newnote.missed = false
     setmetatable(newnote, level.notes.funcs)
     level.notes.list[number] = newnote
 end
 
 function level.notes.funcs:update(dt)
     self.x = self.x - (dt * speed_per_second)
+    if not self.missed and self.x < 27 then
+        self.missed = true
+        level.stats.hp = level.stats.hp - 10
+        level.stats.miss = level.stats.miss + 1
+    end
 end
 
 level.data = {}
