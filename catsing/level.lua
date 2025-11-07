c1120e4180623413006a18aff1680e1251f10372
Meowybot <jorgedidier149@gmail.com>
2025-11-07 14:37:05 -0600

    Actualizar level.lua

--- catsing/level.lua
+++ catsing/level.lua
@@ -8,6 +8,12 @@ level.mdata.secps = level.mdata.secpb / level.mdata.spb
 level.mdata.name = "Tutorial"
 level.mdata.id = "00"
 level.mdata.curstep = 0
+
+local target_x = 0
+local initial_spawn_offset_x = 800
+local total_time_to_target = 4
+local speed_per_second = (initial_spawn_offset_x - target_x) / total_time_to_target
+
 level.notes = {}
 level.notes.list = {}
 level.notes.funcs = {}
@@ -15,12 +21,13 @@ level.notes.funcs = {}
 function level.notes.add(number, step)
     local newnote = {}
     newnote.y = 20
-    newnote.x = step * 10
+    local time_to_hit = step * level.mdata.secps
+    newnote.x = initial_spawn_offset_x + (time_to_hit * speed_per_second)
     newnote.r = 25
     setmetatable(newnote, level.notes.funcs)
     level.notes.list[number] = newnote
 end
 
 function level.notes.funcs:update(dt)
-    self.x = self.x - (dt * 300)
+    self.x = self.x - (dt * speed_per_second)
 end
