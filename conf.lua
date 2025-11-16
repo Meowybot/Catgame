5746bae3fa656a317dbb83c230ee99f524c9bb51
Meowybot <jorgedidier149@gmail.com>
2025-11-16 10:37:26 -0600

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -2,8 +2,4 @@ function love.conf(t)
     t.window.width = 1280
     t.window.height = 720
     t.window.title = "CatSing! for PC"
-    t.window.resizable = true
-    t.window.minwidth = 1270
-    t.window.minheight = 710
-    --changes the config files
 end
